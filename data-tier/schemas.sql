71b189de624e82e7f2e595b0c8539a06cfaed887
Michael K <33135632+MichaelKoiku@users.noreply.github.com>
2023-02-17 12:45:09 -0800

    Update schemas.sql

--- data-tier/schemas.sql
+++ data-tier/schemas.sql
@@ -8,39 +8,37 @@ CREATE TABLE user (
     email VARCHAR (64) UNIQUE NOT NULL,
     password VARCHAR (32) NOT NULL,
     account_type VARCHAR (64) NOT NULL,
-    title varchar(64), 
-    first_name varchar(64), 
-    last_name varchar(64),
-    bio varchar(64), 
-    faculty varchar(64)
+    title  VARCHAR (64), 
+    first_name  VARCHAR (64), 
+    last_name  VARCHAR (64),
+    bio  VARCHAR (64), 
+    department  VARCHAR (64)
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE post (
-    post_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE artifact (
+    artifact_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR (15) NOT NULL,
     title VARCHAR (50) NOT NULL,
     content VARCHAR (255) NOT NULL,
+    external_url VARCHAR (255),
+    category VARCHAR (64),
+    status VARCHAR (64),
+    location VARCHAR (64),
+    artifact_date_time DATETIME,
     date_time_created DATETIME NOT NULL DEFAULT NOW(),
     media_url VARCHAR (255) UNIQUE,
     likes_count INT NOT NULL DEFAULT 0,
-    category varchar(64), 
-    status varchar(64),
-    location varchar(64),
-    artifact_date varchar(64),
-    artifact_time varchar(64),
-    external_url varchar(64)
     PRIMARY KEY(post_id, username),
     FOREIGN KEY (username) REFERENCES user (username)
 );
 
-INSERT INTO user (username, email, password, account_type) VALUES ('admin', 'admin@my.yorku.ca', 'admin123', 'Adminstrator');
-INSERT INTO user (username, email, password, account_type) VALUES ('student1', 'student1@my.yorku.ca', 'student1_123', 'Student');
-INSERT INTO user (username, email, password, account_type) VALUES ('student2', 'student2@my.yorku.ca', 'student2_123', 'Student');
-INSERT INTO user (username, email, password, account_type) VALUES ('professor1', 'professor1@my.yorku.ca', 'professor1_123', 'Professor');
-INSERT INTO user (username, email, password, account_type) VALUES ('professor2', 'professor2@my.yorku.ca', 'professor2_123', 'Professor');
-INSERT INTO user (username, email, password, account_type) VALUES ('organization1', 'organization1@my.yorku.ca', 'organization1_123', 'Organization');
-INSERT INTO user (username, email, password, account_type) VALUES ('organization2', 'organization2@my.yorku.ca', 'organization2_123', 'Organization');
+INSERT INTO user (username, email, password, account_type, first_name, last_name, bio, faculty) VALUES ('jsmith', 'jsmith@my.yorku.ca', 'student1_123', 'Student','John', 'Smith', '2nd Year Computer Science Student and Soccer Lover', 'Electrical Engineering and Computer Science');
+INSERT INTO user (username, email, password, account_type, first_name, last_name, bio, faculty) VALUES ('svalera', 'svalera@my.yorku.ca', 'student2_123', 'Student', 'Samantha', 'Valera', '3rd Year Biomedical Science Student and Art Nerd', 'Biology');
+INSERT INTO user (username, email, password, account_type, first_name, last_name, bio, faculty) VALUES ('johnadmams', 'johnadmams@my.yorku.ca', 'professor1_123', 'Professor', 'John', 'Adams', 'Professor at the Lassonde School of Engineering teaching EECS 3101', 'Electrical Engineering and Computer Science');
+INSERT INTO user (username, email, password, account_type, first_name, last_name, bio, faculty) VALUES ('elizabethparker', 'elizabethparker@my.yorku.ca', 'professor2_123', 'Professor', 'Elizabeth', 'Parker', 'Professor at the Department of Biology teaching BIOL 1000', 'Biology');
+INSERT INTO user (username, email, password, account_type) VALUES ('cshub', 'cshub@my.yorku.ca', 'organization1_123', 'Organization');
+INSERT INTO user (username, email, password, account_type) VALUES ('yfs', 'yfs@my.yorku.ca', 'organization2_123', 'Organization');
 
 
 INSERT INTO post (username, title, content, likes_count) VALUES ('professor1', 
