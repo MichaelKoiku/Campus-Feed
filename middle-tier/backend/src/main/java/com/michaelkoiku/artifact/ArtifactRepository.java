5aa630a9902473407aac4e30bfbdc80c77d4b115
Michael K <33135632+MichaelKoiku@users.noreply.github.com>
2023-02-18 23:01:01 -0500

    "add user & artifact. implement singleton design pattern"

---
+++ middle-tier/backend/src/main/java/com/michaelkoiku/artifact/ArtifactRepository.java
@@ -0,0 +1,7 @@
+package com.michaelkoiku.artifact;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtifactRepository extends JpaRepository<Artifact, Long>{
+
+}
