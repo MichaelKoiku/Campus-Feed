65ff4028a51b81f7e28be14bc8ecab3c36725542
Michael K <33135632+MichaelKoiku@users.noreply.github.com>
2023-02-19 03:39:00 -0500

    "add controller. add foundation for command design pattern implementation""

--- middle-tier/backend/src/main/java/com/michaelkoiku/controller/StudentController.java
+++ middle-tier/backend/src/main/java/com/michaelkoiku/controller/StudentController.java
@@ -1,5 +1,21 @@
 package com.michaelkoiku.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student/")
 public class StudentController {
+	
+	@GetMapping("home")
+	public ResponseEntity<String> getHomePage() {
+		//Step 1. Invoke Command
+		
+		//Step 2. Return Command Response of type ResponseEntity
+		return new ResponseEntity<String>("Home Page End Point Hit.", HttpStatus.OK);
+	}
 
 }
--- middle-tier/backend/src/main/java/com/michaelkoiku/factory/UserFactory.java
+++ middle-tier/backend/src/main/java/com/michaelkoiku/factory/UserFactory.java
@@ -2,8 +2,18 @@ package com.michaelkoiku.factory;
 
 import com.michaelkoiku.user.User;
 
-//This class implements the Factory Method design pattern
+//This class implements the Factory Method and Singleton design pattern
 public class UserFactory extends Factory {
+	private static UserFactory instance;
+	
+	private UserFactory() {}
+	
+	public static UserFactory getInstance() {
+		if(instance == null) {
+			instance = new UserFactory();
+		}
+		return instance;
+	}
 
 	@Override
 	public Entity createEntity() {
